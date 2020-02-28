9020883db60a111b567d5a1e16b28dc3c51fa074
gullasneha <61595454+gullasneha@users.noreply.github.com>
2020-02-28 21:44:57 +0100

    Update ec2.tf

--- terraform_script/ec2.tf
+++ terraform_script/ec2.tf
@@ -1,1 +1,16 @@
+module "ec2_cluster" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 2.0"
+  ami                    = "ami-01c94064639c71719"
+  instance_type          = "t2.micro"
+  instance 		 = 2
+  name                   = "twec2"
+  
+  vpc_security_group_ids = ["sg-0ec878d53f19307a3"]
+  subnet_id              = "subnet-0754c0403c263f982"
 
+  tags = {
+    Terraform   = "true"
+    Environment = "test"
+  }
+}
